987073544e791c7cbe0f396d8ff1dd7f31a13257
Francois Lecroart <peernohell@gmail.com>
2020-05-23 09:49:26 +0200

    add report-viewer in base_url

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'/report-viewer',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
