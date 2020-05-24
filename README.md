c448a3e7d984d3c20fa1531d8535c62939a15be7
Francois Lecroart <peernohell@gmail.com>
2020-05-24 20:34:01 +0200

    Build

--- README.md
+++
@@ -1,8 +0,0 @@
-# TODO
-
- * Improve header section
-
-# Thanks
-
-Thanks to [Conduitry]https://github.com/Conduitry/sapper-gh-pages) for his sapper-gh-pages script.
-Thanks to [Vannsl](https://dev.to/vannsl) for his [article](https://dev.to/vannsl/statically-generated-website-with-svelte-and-sapper-5bi7) on how to build a static website with Svelte and Sapper
--- src/routes/utils.js
+++
@@ -1,9 +0,0 @@
-
-// underscore string function https://github.com/esamattis/underscore.string
-export const underscored = str => String(str).trim().replace(/([a-z\d])([A-Z]+)/g, '$1_$2').replace(/[-\s]+/g, '_').toLowerCase();
-export const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
-export const humanize = str => capitalize(underscored(str).replace(/_id$/, '').replace(/_/g, ' ').trim());
-
-// export underscored
-// export capitalize;
-// export humanize;
